335f771d3361a0de838dbc4259a61cae33bd0189
Aleksander Mirowsky <aleks@pop-os.localdomain>
2021-11-18 15:07:54 -0300

    [config] removed .gradle and .idea folders from cache and further testing based on kotlin docs reading

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-
+import javax.lang.model.element.Element
 
 fun add(vararg args: Int): Int = args.reduce {acc, next -> acc + next }
 
@@ -11,6 +11,21 @@ fun main(args: Array<String>) {
     val allTheNumbs = add(1, 2, 3)
 
 
+
+    List<Int>(10, init = {index -> index })
     println(allTheNumbs)
 
+
+    fun lambdaSumAnon(vararg args: Int): Int = args.reduce { acc, list -> acc + list }
+
+
+    val lambdaSum = {ars: List<Int> -> ars.reduce {acc, next ->  acc + next} }
+
+    val lambdaFoldSum = listOf(2, 3, 4).fold(0) { acc, i ->  acc + i }
+
+    println(lambdaSum(listOf(1, 2, 3)))
+
+    println(lambdaFoldSum)
+
+    println(lambdaSumAnon(10, 20, 30))
 }
