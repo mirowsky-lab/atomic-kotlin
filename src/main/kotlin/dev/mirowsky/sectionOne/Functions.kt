1b1e17c4ecd1ca85d7dadb2a067a020d5272720a
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-11-19 00:15:27 -0300

    [bugfix] novelty pseudo-random generator bug because i forgot order of operations matter

--- src/main/kotlin/dev/mirowsky/sectionOne/Functions.kt
+++ src/main/kotlin/dev/mirowsky/sectionOne/Functions.kt
@@ -9,7 +9,5 @@ fun add(vararg args: Int) {args.reduce { acc, i -> acc + i  }}
 fun subtract(vararg  args: Int) {args.reduce { accumulator, item -> accumulator + item}}
 
 fun randomRange(from: Int, to: Int): Int {
-
-   return Math.floor(Math.random() + to).toInt() + from
-
+   return floor(Math.random() * to).toInt() + from
 }
