0994de5884ce63fdb38aaac129da77bc93f93921
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-11-22 01:51:47 -0300

    atomic testing bugfix

--- src/main/kotlin/dev/mirowsky/sectionTwo/atomicTest/AtomicTest.kt
+++ src/main/kotlin/dev/mirowsky/sectionTwo/atomicTest/AtomicTest.kt
@@ -1,14 +1,16 @@
 package dev.mirowsky.sectionTwo.atomicTest
-import kotlin.math.abs
 import kotlin.reflect.KClass
-
+import kotlin.math.*
 
 const val ERROR_TAG = "[Error]: "
-
-
-private fun <L, R> test(actual: L, expected: R, checkEquals: Boolean = true, predicate: () -> Boolean) {
+private fun <L, R> test(
+    actual: L,
+    expected: R,
+    checkEquals: Boolean = true,
+    predicate: () -> Boolean
+) {
     println(actual)
-    if (predicate()) {
+    if (!predicate()) {
         print(ERROR_TAG)
         println("$actual " +
                 (if (checkEquals) "!=" else "==") +
@@ -18,75 +20,79 @@ private fun <L, R> test(actual: L, expected: R, checkEquals: Boolean = true, pre
 
 infix fun Any.eq(rval: String) {
     test(this, rval) {
-        toString().trim() === rval.trimIndent()
+        toString().trim() == rval.trimIndent()
     }
 }
-
-
-infix fun <TType> TType.eq(rval: TType) {
-    test(this, rval) { this == rval}
+/**
+ * Verifies this object is equal to `rval`.
+ */
+infix fun <T> T.eq(rval: T) {
+    test(this, rval) {
+        this == rval
+    }
 }
-
-infix fun <TType> TType.neq(rval: TType) {
-    test(this, rval, checkEquals = false) { this != rval}
+/**
+ * Verifies this object is != `rval`.
+ */
+infix fun <T> T.neq(rval: T) {
+    test(this, rval, checkEquals = false) {
+        this != rval
+    }
 }
 
 infix fun Double.eq(rval: Double) {
-    test(this, rval) { abs(this - rval) < 0.0000001 }
+    test(this, rval) {
+        abs(this - rval) < 0.0000001
+    }
 }
 
+class CapturedException(
+    private val exceptionClass: KClass<*>?,
 
-
-class CapturedException(private val exceptionClass: KClass<*>?, private val actualMessage: String) {
-
-    private val fullMessage: String get() {
-        val className = exceptionClass?.simpleName ?: ""
+    private val actualMessage: String
+) {
+    private val fullMessage: String
+    get() {
+        val className =
+            exceptionClass?.simpleName ?: ""
         return className + actualMessage
     }
-
     infix fun eq(message: String) {
         fullMessage eq message
     }
-
     infix fun contains(parts: List<String>) {
-        if (parts.any() { it !in fullMessage}) {
+        if (parts.any { it !in fullMessage }) {
             print(ERROR_TAG)
             println("Actual message: $fullMessage")
             println("Expected parts: $parts")
         }
     }
-
     override fun toString() = fullMessage
 }
 
-
-
-
-fun capture(f: () -> Unit): CapturedException =
+fun capture(f:() -> Unit): CapturedException =
     try {
         f()
-        CapturedException(null, "$ERROR_TAG Expected an exception")
+        CapturedException(null,
+            "$ERROR_TAG Expected an exception")
     } catch (e: Throwable) {
         CapturedException(e::class,
             (e.message?.let { ": $it" } ?: ""))
     }
-
 
 object trace {
     private val trc = mutableListOf<String>()
     operator fun invoke(obj: Any?) {
         trc += obj.toString()
     }
+
     infix fun eq(multiline: String) {
         val trace = trc.joinToString("\n")
-
-        val expected = multiline.trimIndent().replace("\n", " ")
-
+        val expected = multiline.trimIndent()
+            .replace("\n", " ")
         test(trace, multiline) {
             trace.replace("\n", " ") == expected
         }
-
         trc.clear()
     }
 }
-
--- src/main/kotlin/dev/mirowsky/sectionTwo/testing/TestingExample.kt
+++ src/main/kotlin/dev/mirowsky/sectionTwo/testing/TestingExample.kt
@@ -1,2 +1,21 @@
 package dev.mirowsky.sectionTwo.testing
 
+import dev.mirowsky.sectionTwo.atomicTest.*
+
+fun main() {
+
+
+    val v1 = 11
+    val v2 = "Ontology"
+
+
+    v1 eq 11
+    v2 eq "Ontology"
+
+
+    v2 neq "Epistemology"
+
+
+    v2 eq "Hypotenuse"
+
+}
